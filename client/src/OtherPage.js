f7f56b6421714bcd1acf125db8d8e666673e8e8c
Patryk Kazmierak <patryk.kazmierak@pollub.edu.pl>
2023-01-25 11:18:26 +0100

    Source files added

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h2>Aplikacja pkmultifib</h2>
+      <h2>Autor Patryk Kaźmierak</h2>
+      <h2>Grupa I2S 2.2</h2>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
